8aa5442f2d496499b056ea01852d2475cf719948
gensakusiya <homoogenecus@gmail.com>
2019-02-23 16:15:29 +0300

    passed port to app

--- config/index.js
+++ config/index.js
@@ -1,11 +1,13 @@
 const env = process.env.NODE_ENV;
+const port = process.env.PORT;
 const dev = env !== "production";
 
 const path = dev ? `./env/dev.json` : `./env/production.json`;
 
-const config = require(path);
+const config = require("./env/production");
 
 module.exports = {
   DEV: dev,
-  ...config
+  ...config,
+  PORT: port
 };
